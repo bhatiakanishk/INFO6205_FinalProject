be92e02828c053b5f6434b7173ee71c983ead56f
Raj Mehta <raj@akoya.com>
2022-04-16 13:22:12 -0400

    Add human move functionality on board

--- main/human.py
+++ main/human.py
@@ -2,5 +2,6 @@ class Human:
     def __init__(self):
         pass
 
-    def make_move_on_board(self):
+    def human_move(self):
         input_move = input("Make a move on the board:")
+        return input_move
