df19ff245ea8db85afa59b647c285ac33d191050
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-04-16 11:38:52 -0400

    Update game_board.py

--- main/game_board.py
+++ main/game_board.py
@@ -14,7 +14,24 @@ class Game_Board:
         print("-------")
 
     def win_condition(self):
-        pass
+        win_condition = False
+        if(self.game_board[0]!=' ' and self.game_board[0] == self.game_board[1] == self.game_board[2]):
+            win_condition = True
+        elif(self.game_board[0]!=' ' and self.game_board[0] == self.game_board[3] == self.game_board[6]):
+            win_condition = True
+        elif(self.game_board[0]!=' ' and self.game_board[0] == self.game_board[4] == self.game_board[8]):
+            win_condition = True
+        elif(self.game_board[4]!=' ' and self.game_board[1] == self.game_board[4] == self.game_board[7]):
+            win_condition = True
+        elif(self.game_board[4]!=' ' and self.game_board[3] == self.game_board[4] == self.game_board[5]):
+            win_condition = True
+        elif(self.game_board[4]!=' ' and self.game_board[2] == self.game_board[4] == self.game_board[6]):
+            win_condition = True
+        elif(self.game_board[8]!=' ' and self.game_board[2] == self.game_board[5] == self.game_board[8]):
+            win_condition = True
+        elif(self.game_board[8]!=' ' and self.game_board[6] == self.game_board[7] == self.game_board[8]):
+            win_condition = True
+        return win_condition
 
     def draw_condition(self):
         pass
