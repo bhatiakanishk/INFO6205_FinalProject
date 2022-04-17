40d811be5cf673e49d9910b4a3e9d071ff0053cc
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-04-17 17:45:07 -0400

    Update human.py

--- main/human.py
+++ main/human.py
@@ -5,3 +5,11 @@ class Human:
     def human_move(self):
         input_move = input("Make a move on the board:")
         return input_move
+    def win_result(self):
+        print("Human Player Wins.")
+    
+    def draw_result(self):
+        print("Game Draw.")
+
+    def lose_result(self):
+        print("Human Player Loses.")
