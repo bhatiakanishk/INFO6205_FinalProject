447a9556ba32ef6bfcadd881241e046155a4128b
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-04-22 18:24:39 -0400

    Update test_game_board.py

--- test/test_game_board.py
+++ test/test_game_board.py
@@ -7,5 +7,27 @@ import os
 sys.path.append(os.getcwd())
 from main import game_board
 
-def test_win_condition(self):
-    print("test")
+class GameBoardTest(unittest.TestCase):
+    
+    def test_win_condition(self):
+        game_board_test = game_board.Game_Board()
+        test_board = ['X', 'X', 'X', ' ', ' ', ' ', ' ', ' ', ' ',]
+        game_board_test.set_game_board(test_board)
+        self.assertTrue(game_board_test.win_condition())
+
+    def test_draw_condition(self):
+        game_board_test = game_board.Game_Board()
+        test_board = ['X', 'X', '0', 'X', 'X', '0', '0', '0', 'X',]
+        game_board_test.set_game_board(test_board)
+        self.assertTrue(game_board_test.draw_condition())
+
+    def test_make_move_on_board(self):
+        game_board_test = game_board.Game_Board()
+        game_board_test.make_move_on_board(0, "X")
+        received_game_board = game_board_test.get_game_board()
+        self.assertEquals(received_game_board[0], "X")
+
+    def test_is_move_valid(self):
+        game_board_test = game_board.Game_Board()
+        received_game_board = game_board_test.get_game_board()
+        self.assertFalse(game_board_test.is_move_valid(10, game_board_test))
