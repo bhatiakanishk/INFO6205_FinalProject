badeb4adf5415f6fdc906d2e3519fb94aa75dc11
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-04-22 16:56:06 -0400

    testing

---
+++ test/test_game_board.py
@@ -0,0 +1,11 @@
+# This will contain unit tests for the Game Board
+
+import pytest
+import unittest
+import sys
+import os
+sys.path.append(os.getcwd())
+from main import game_board
+
+def test_win_condition(self):
+    print("test")
