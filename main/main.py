dc6fd3aee9fc2862b0928e69d45ec34fcd8fade1
Raj Mehta <raj@akoya.com>
2022-04-23 09:27:15 -0400

    Fixed training gameplay not making draws

--- main/main.py
+++ main/main.py
@@ -2,49 +2,99 @@ from concurrent.futures import ThreadPoolExecutor
 from game_board import Game_Board
 from menace import Menace
 from human import Human
+import datetime
+
+number_of_plays = 0
+number_of_wins = 0
+number_of_draws = 0
+number_of_loses = 0
+log_file = open('log.txt', 'a')
+
+def logging(current_time, result):
+    log_file = open('log.txt', 'a')
+    log_file.write("Time: " + str(current_time) + " | Result: " + str(result) + "\n")
 
 #This method uses human optimal strategy for opponent
 def training():
     game_board_training = Game_Board()
-    #game_board_training.display_board()
+    global number_of_plays
+    global number_of_wins
+    global number_of_draws
+    global number_of_loses
+    number_of_plays += 1
+    game_board_training.display_board()
 
     while True:
         #Determine move of menace based on training
         menace_move = menace.move_to_make(game_board_training)
+        print("Menace tries to make:")
+        print(menace_move)
         game_board_training.make_move_on_board(menace_move, "X")
         
-        #print("Move Made By Menace:")
-        #game_board_training.display_board()
+        print("Move Made By Menace:")
+        game_board_training.display_board()
 
         if game_board_training.win_condition():
             menace.win_result()
+            number_of_wins += 1
             human.lose_result()
+
+            current_time = datetime.datetime.now()
+            result = "Menace Win"
+            logging(current_time, result)
+
             break
             
         if game_board_training.draw_condition():
             menace.draw_result()
+            number_of_draws += 1
+
+            current_time = datetime.datetime.now()
+            result = "Menace Draw"
+            logging(current_time, result)
+            
             break
 
 
         #Take move input from human
         human_move = human.human_move(game_board_training)
+        print("Human strategy tries to make:")
+        print(human_move)
         #Validate if move is valid, i.e. between 0 and 8 and cell is empty
         if game_board_training.is_move_valid(human_move, game_board_training):
             game_board_training.make_move_on_board(human_move, "O")
-            #game_board_training.display_board()
+            game_board_training.display_board()
 
             if game_board_training.win_condition():
                 human.win_result()
                 menace.lose_result()
+                number_of_loses += 1
+
+                current_time = datetime.datetime.now()
+                result = "Menace Lose"
+                logging(current_time, result)
+
                 break
 
             if game_board_training.draw_condition():
                 menace.draw_result()
+                number_of_draws += 1
+
+                current_time = datetime.datetime.now()
+                result = "Menace Draw"
+                logging(current_time, result)
+
                 break
         else:
             print("Invalid move.")
             menace.win_result()
             human.lose_result
+            number_of_wins += 1
+
+            current_time = datetime.datetime.now()
+            result = "Menace Win"
+            logging(current_time, result)
+
             break
 
 #This method takes human input for gameplay. To be used after training
@@ -101,11 +151,26 @@ if __name__ == '__main__':
     
     #Multithreading implementation
     processes = []
-    with ThreadPoolExecutor(max_workers=10) as executor:
-        for i in range(300):
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        for i in range(100):
             processes.append(executor.submit(training()))
-    print("Training complete")    
-
+    print("Training complete")
+    print("Number of games: " + str(number_of_plays))
+    print("Number of wins: " + str(number_of_wins))
+    print("Number of draws: " + str(number_of_draws))
+    print("Number of loses: " + str(number_of_loses))
+    win_percentage = (100*number_of_wins)/number_of_plays
+    lose_percentage = (100*number_of_loses)/number_of_plays
+    print("Win Percentage: " + str(win_percentage))
+    print("Lose Percentage: " + str(lose_percentage))
+    log_file = open('log.txt', 'a')
+    log_file.write("Number of games: " + str(number_of_plays) + "\n")
+    log_file.write("Number of wins: " + str(number_of_wins) + "\n")
+    log_file.write("Number of draws: " + str(number_of_draws) + "\n")
+    log_file.write("Number of loses: " + str(number_of_loses) + "\n")
+    log_file.write("Win Percentage: " + str(win_percentage) + "\n")
+    log_file.write("Lost Percentage: " + str(lose_percentage) + "\n")
+    log_file.write("-------------------------------------------------" + "\n")
     print("-------------------------------------------------") 
 
     print("Gameplay now:")
